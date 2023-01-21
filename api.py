77c9f0c45b126d3540606b78c5fcb20174044744
unknown <girishbablani546@gmail.com>
2023-01-21 23:20:18 +0530

    Build error

--- api.py
+++ api.py
@@ -1,10 +1,10 @@
 from flask import Flask,jsonify
 
 
-app = Flask(__name__)
+app = Flask(__name__)   #noqa : F821
 @app.route('/')
 def home():
-    return jsonify({"message":"This api is working"})
+    return jsonify({"message":"This api is working"}) 
 
-if __name__=="__main__":
-    app.run(debug=True)    
+if __name__=="__main__":   #noqa : F821
+    app.run(debug=False)    
