6cf8ebdd9f305575a0d17498fcd26a3108ef25cb
unknown <girishbablani546@gmail.com>
2023-01-22 01:28:44 +0530

    remove noqa comment

--- api.py
+++ api.py
@@ -1,10 +1,10 @@
 from flask import Flask,jsonify
 
 
-app = Flask(__name__)   #noqa : F821
+app = Flask(__name__)   
 @app.route('/')
 def home():
     return jsonify({"message":"This api is working"}) 
 
-if __name__=="__main__":   #noqa : F821
+if __name__=="__main__":   
     app.run(debug=False)    
